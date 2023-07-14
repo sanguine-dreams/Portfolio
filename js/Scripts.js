9602f15d3cade6a21f1847fd415c02555a931b16
sanguine-dreams <sara0dhahir@gmail.com>
2023-07-14 14:29:46 +0300

    Disco.

---
+++ js/Scripts.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+var y = document.getElementById("hide");
+y.style.display = "none";
+
+}
